fccb0a8d53ceab553d00dc9ca8eb71b940cc310d
Anastasiia <anastasiiayaroshynska@gmail.com>
2020-03-16 02:40:07 +0200

    This file was uploaded by mistake

--- newfile.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import json
-#import time
-starttime = time.time()
-#while True:
-	data = urllib.request.urlopen('http://resources.finance.ua/ru/public/currency-cash.json')
-    ParsedData = json.loads(data.read())
-    print(ParsedData["organizations"][0]["currencies"]["EUR"])
-   # time.sleep(5.0 - ((time.time() - starttime) % 5.0)
